1c115244a03293fbd630e663ca979f321cb88539
fernandobueno <buenopt@hotmail.com>
2020-09-19 18:27:32 -0300

    Versão 0.31 - Ajuste campo número de funcionários que foi removido

--- Controller/ControllerCadastro.php
+++ Controller/ControllerCadastro.php
@@ -6,7 +6,7 @@ $dataAtual = date('Y-m-d H:i:s');
 #Cadastro das informações da empresa
 $Crud = new ClassCrud();
 try {
-    $values = "'" . $nomeEmpresa . "','" . $emailEmpresa . "','" . $fkgestorEmpresa . "','" . $fksegEmpresa . "','" . $fkqtdeFuncEmpresa . "','" . $fkfatEmpresa . "','" . $dataAtual . "'";
+    $values = "'" . $nomeEmpresa . "','" . $emailEmpresa . "','" . $fkgestorEmpresa . "','" . $fksegEmpresa . "','" . $fkfatEmpresa . "','" . $dataAtual . "'";
     $parametros = ("`nomeEmpresa`, `emailEmpresa`, `fkgestorEmpresa`, `fksegEmpresa`, `fkqtdeFuncEmpresa`, `fkfatEmpresa`, `dtCadEmpresa`");
     $idEmpresa = $Crud->insertDB("tbempresas", $parametros, $values);
     session_start();
